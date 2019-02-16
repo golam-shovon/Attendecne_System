aaf7950490310bc6f2fcdc4f27c2fb23c2305342
golam-mohammd-shovon <38292502+golam-shovon@users.noreply.github.com>
2019-02-16 19:19:30 +0600

    <attecndecne system created added lgin logout using django built in auth system>

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!K:\Attendecne_System\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ atn_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AtnSystemConfig(AppConfig):
+    name = 'atn_system'
